7819776ce891567d91cd20f49d0091aeeff6e149
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-04-30 12:23:29 +0200

    Two animated functions in one graph

---
+++ animated_plots/example03_sin_cos.py
@@ -0,0 +1,49 @@
+# Knihovny Numpy a matplotlib
+#
+# Třetí demonstrační příklad:
+# - vykreslení animovaného průběhů funkcí sin a cos
+#   do jediného grafu
+
+import numpy as np
+import matplotlib.pyplot as plt
+from moviepy.editor import VideoClip
+from moviepy.video.io.bindings import mplfig_to_npimage
+
+# parametry obrázků / rámců
+WIDTH = 400
+HEIGHT = 300
+DPI = 100
+
+# parametry animace
+DURATION = 10
+FPS = 15
+
+# hodnoty na x-ové ose
+x = np.linspace(0, 2 * np.pi, 100)
+
+# vytvoření objektu reprezentujícího průběh funkce
+# + nastavení rozlišení obrázku (resp. jednotlivých rámců)
+fig, axis = plt.subplots(figsize=(1.0 * WIDTH / DPI, 1.0 * HEIGHT / DPI), dpi=DPI)
+
+def make_frame(t):
+    axis.clear()
+
+    # offset v rozmezí 0 .. 2*Pi
+    offset = 2 * np.pi * t / DURATION
+
+    # hodnoty na y-ové ose: první funkce
+    y1 = np.sin(x + offset)
+
+    # hodnoty na y-ové ose: druhá funkce
+    y2 = np.cos(x - offset)
+
+    # vykreslení průběhu funkce
+    axis.plot(x, y1)
+    axis.plot(x, y2)
+
+    # konverze na objekt typu "frame"
+    return mplfig_to_npimage(fig)
+
+
+animation = VideoClip(make_frame, duration=DURATION)
+animation.write_gif('sin_cos.gif', fps=FPS)
