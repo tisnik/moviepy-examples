1de9ac9d3cf5459c046239682dbc0ccdd079c9e6
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-08-01 10:24:27 +0200

    Video processing example #5 - video resizing

---
+++ video_processing/05_resize_to_larger.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+#
+#  (C) Copyright 2018  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
+from moviepy.editor import VideoClip, VideoFileClip
+from moviepy.video.fx.all import *
+import numpy
+
+# nacteni video klipu
+clip = VideoFileClip('input_video.mp4', audio=False)
+
+# novy klip predstavujici video zvetsene na 150% puvodni velikosti
+resized_clip = clip.fx(resize, 1.50)
+
+# ulozeni video klipu do jineho souboru
+resized_clip.write_videofile('05_output_larger.ogv', bitrate='700000')
