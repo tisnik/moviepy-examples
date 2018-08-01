4dd70a114442c509ce34c88e176a095fe930c84b
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-08-01 10:27:41 +0200

    Video processing example #11 - simple motion blur

---
+++ video_processing/11_motion_blur.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+#
+#  (C) Copyright 2018  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
+from moviepy.editor import ImageClip, VideoClip, VideoFileClip, CompositeVideoClip
+from moviepy.video.fx.all import *
+
+# nacteni video klipu
+clip = VideoFileClip('input_video.mp4', audio=False)
+
+# aplikace rozmazani pohybem
+blurred_clip = clip.fx(supersample, 0.25, 10)
+
+# ulozeni video klipu do jineho souboru
+blurred_clip.write_videofile('11_motion_blur.ogv', bitrate='600000')
