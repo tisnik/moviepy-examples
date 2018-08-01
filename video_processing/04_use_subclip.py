92295cbc0b5fdf16ea1f1a7c8dbd3a1483b4643a
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-08-01 10:24:00 +0200

    Video processing example #4 - subclipping

---
+++ video_processing/04_use_subclip.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+#
+#  (C) Copyright 2018  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
+from moviepy.editor import VideoClip, VideoFileClip
+import numpy
+
+# nacteni video klipu
+clip = VideoFileClip('input_video.mp4', audio=False)
+
+# novy objekt reprezentujici video klip od 5 do 10 sekundy
+sub_clip = clip.subclip(5.0, 10.0)
+
+# ulozeni video klipu do jineho souboru
+sub_clip.write_videofile('04_output_subclip.ogv', bitrate='700000')
